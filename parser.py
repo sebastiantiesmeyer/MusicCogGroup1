8fb553db82c1f10ac5d87864de9226fa37490020
Constantin1225561 <trillian42>
2017-03-16 12:55:21 +0100

    parse genre and count songs

--- parser.py
+++ parser.py
@@ -3,6 +3,9 @@ import glob
 #iterate through files
 for filename in glob.iglob('data/*.txt'):
     print("### Parsing file " + filename)
+    genre = filename[5:len(filename)-4]
+    print("### Genre: "+ genre)
+    numberOfSongs = 0
     file = open(filename)
     lines = file.readlines()
     i = 0
@@ -11,10 +14,11 @@ for filename in glob.iglob('data/*.txt'):
     while i < len(lines):
         artist = lines[i][3:].strip()
         title = lines[i+1][3:].strip()
+        numberOfSongs = numberOfSongs + 1
         print (artist + " - " + title)
         i = i + 2
         while i < len(lines) and not lines[i].startswith("###"):
             line = lines[i].strip()
             #print (line)
             i = i + 1
-
+    print("### "+ str(numberOfSongs) + " songs found for "+ genre +"\n")

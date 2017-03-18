14cad2257fa14e4d9f494379328af591cf394a99
Constantin1225561 <trillian42>
2017-03-18 20:58:21 +0100

    add basshunter songs

--- classifier.py
+++ classifier.py
@@ -1,1 +1,16 @@
-#TODO
+import pandas as pd
+
+#Load data.csv
+df=pd.read_csv('data.csv', sep=',',header=None, names=['Genre', 'Artist', 'Title', 'UniqueLinesRatio'])
+X, y = df.iloc[1:,3:], df.iloc[1:, 1]
+
+print(X)
+print(y)
+
+#Classify
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+
+clf = NearestCentroid()
+clf.fit(X, y)
+NearestCentroid(metric='euclidean', shrink_threshold=None)
+print(clf.predict([[1]]))
